6f8227f901a85ae1f265c461b1706b83f7d66e47
SkyBeastMC <skybeastmc@gmail.com>
2018-06-02 22:19:41 +0200

    Reaction decorator (Close #2)

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -2,6 +2,7 @@ import command from './command'
 import configurable from './configurable'
 import on from './on'
 import restrict from './restrict'
+import react from './react'
 
 export * from './restrict'
-export { command, configurable, on, restrict }
+export { command, configurable, on, restrict, react }
