ac43854b81da32394e249c82c9449cd2c8c2e1e6
SkyBeastMC <skybeastmc@gmail.com>
2018-10-31 02:05:54 +0100

    better data storage

--- src/Migi.js
+++ src/Migi.js
@@ -1,60 +1,75 @@
 import { Client } from 'discord.js'
 
-export const MODULE_DATA = Symbol('module_data')
-export const LISTENER_DATA = Symbol('listener_data')
-const MIGI_DATA = Symbol('migi_data')
-
-export function getModuleData(module) {
-	return (typeof module === 'function' ? module : module.constructor)[
-		MODULE_DATA
-	]
-}
+const MIGI_DATA = Symbol('migi')
 
 export function isModule(instance) {
-	return !!(instance.constructor && instance.constructor[MODULE_DATA])
+	return !!(instance.constructor && instance.constructor[MIGI_DATA])
 }
 
-export function getListenerData(handler) {
-	return handler[LISTENER_DATA]
+export function getMigiData(obj) {
+	return obj[MIGI_DATA]
 }
 
 export default class Migi extends Client {
 	constructor({ discordOptions, root = process.cwd() } = {}) {
 		super(discordOptions)
-		this.root = root
+		this._root = root
 		this._modules = []
 	}
 
+	get root() {
+		return this._root
+	}
+
 	loadModule(ModuleConstructor) {
-		// public module data
-		ModuleConstructor[MODULE_DATA] = {
-			[MIGI_DATA]: { listeners: [], instance: null },
+		const migi = this
+
+		// init module data
+		ModuleConstructor[MIGI_DATA] = {
+			_listeners: [],
+			_instance: null,
 			get listeners() {
-				return Object.freeze(this[MIGI_DATA].listeners.slice())
+				return Object.freeze(this._listeners.slice())
 			},
 			get instance() {
-				return this[MIGI_DATA].instance
+				return this._instance
+			},
+			get loaded() {
+				return !!this._instance
 			},
 			get name() {
 				return ModuleConstructor.name
+			},
+			get migi() {
+				return migi
 			}
 		}
 
-		const moduleInstance = new ModuleConstructor(this)
-		ModuleConstructor[MODULE_DATA].instance = moduleInstance
+		const module = new ModuleConstructor(this)
+		getMigiData(ModuleConstructor)._instance = module
 
-		this._modules.push(moduleInstance)
-		this.emit('moduleLoad', ModuleConstructor, moduleInstance)
+		// add module
+		this._modules.push(module)
+		this.emit('moduleLoad', module)
 
-		return moduleInstance
+		return module
 	}
 
 	unloadModule(module) {
 		if (!isModule(module) || !this.isModuleLoaded(module))
 			throw new Error('instance must a loaded module instance')
 
-		getModuleData(module)[MIGI_DATA].instance = null
+		const migiData = getMigiData(module.constructor)
+		migiData._instance = null // set unloaded
 
+		// remove listeners
+		migiData._listeners.forEach(listener => {
+			const { event } = getMigiData(listener)
+			this.emit('listenerRemove', module, event, listener)
+			this.removeListener(event, listener)
+		})
+
+		// remove module
 		this.emit('moduleUnload', module)
 		this._modules.remove(module)
 	}
@@ -66,26 +81,28 @@ export default class Migi extends Client {
 		if (typeof listener !== 'function')
 			throw new Error('listener must be a function')
 
-		// public handler data
-		listener[LISTENER_DATA] = {
+		// init listener data
+		listener[MIGI_DATA] = {
 			get module() {
 				return module
 			},
 			get event() {
 				return event
+			},
+			get handler() {
+				return listener
 			}
 		}
 
-		this.removeListener
-
-		getModuleData(module)[MIGI_DATA].listeners.push(listener)
-		this.emit('listenerAdd', listener)
+		// add listener
+		getModuleData(module.constructor)._listeners.push(listener)
+		this.emit('listenerAdd', module, event, listener)
 		this.on(event, listener)
 	}
 
 	isModuleLoaded(module) {
 		if (!isModule(module)) throw new Error('module must be a module instance')
 
-		return !!getModuleData(module).instance
+		return !!getModuleData(module.constructor).instance
 	}
 }
